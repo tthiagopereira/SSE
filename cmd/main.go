88495812b72bdac5365bac4d341ef592a5f55364
THIAGO PEREIRA DOS SANTOS <thiago.pereira@tatic.com.net>
2023-04-01 23:07:39 -0400

    feat: aplication exemple sse

---
+++ cmd/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+
+	"sse/pkg/rabbitmq"
+)
+
+func main() { //T1
+	out := make(chan amqp.Delivery)
+	rabbitmqChannel, err := rabbitmq.OpenChannel()
+	if err != nil {
+		panic(err)
+	}
+	go rabbitmq.Consumer("msgs", rabbitmqChannel, out) // T2
+
+	http.HandleFunc("/sse", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/event-stream")
+		w.Header().Set("Cache-Control", "no-cache")
+		w.Header().Set("Connection", "keep-alive")
+		for m := range out {
+			fmt.Fprintf(w, "event: message\n")
+			fmt.Fprintf(w, "data: %s\n\n", m.Body)
+			w.(http.Flusher).Flush()
+		}
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "html/index.html")
+	})
+	http.ListenAndServe(":3002", nil)
+}
